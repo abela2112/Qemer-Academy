4124f0a6701afe3b02f83c8096a768aed6fed998
Abel Ayalew Abebe <103965027+abela2112@users.noreply.github.com>
2024-08-06 12:15:32 +0300

    update schema

--- app/(dashboard)/(routes)/teacher/courses/_components/Attachment.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_components/Attachment.tsx
@@ -10,7 +10,7 @@ type Props = {
 import FileUpload from "@/components/file-upload";
 import { Button } from "@/components/ui/button";
 import { Attachment, Course } from "@prisma/client";
-import { ImageIcon, Pencil, PlusCircle } from "lucide-react";
+import { File, ImageIcon, Loader2, Pencil, PlusCircle, X } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
@@ -30,8 +30,21 @@ const CourseAttachmentForm = ({ intialData, courseId }: Props) => {
       toast.error("something went wrong");
     }
   };
+  const onDelete = async (id: string) => {
+    try {
+      setDeletingId(id);
+      await axios.delete(`/api/course/${courseId}/attachment/${id}`);
+      toast.success("Attachment deleted successfully");
+      router.refresh();
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setDeletingId(null);
+    }
+  };
   const toggleEdit = () => setIsEditting((current) => !current);
   const [isEditting, setIsEditting] = useState(false);
+  const [deletingId, setDeletingId] = useState<string | null>();
   return (
     <div className="mt-6 bg-slate-100 rounded-md border p-4">
       <div className="font-medium flex item-center justify-between">
@@ -59,14 +72,31 @@ const CourseAttachmentForm = ({ intialData, courseId }: Props) => {
         // </div>
       )}
 
-      {!isEditting && intialData.image && (
-        <div className="aspect-video rounded-md mt-2">
-          <Image
-            src={intialData.image!}
-            fill
-            alt="Course Image"
-            className="object-cover rounded-md"
-          />
+      {!isEditting && intialData.attachment.length > 0 && (
+        <div className="space-y-2">
+          {intialData.attachment.map((attachment) => (
+            <div
+              key={attachment.id}
+              className="flex items-center p-3 bg-sky-100 border-sky-200 border text-sky-700 rounded-md"
+            >
+              <File className="h-4 w-4 mr-2 flex-shrink-0" />
+              <p className="text-xs line-clamp-1">{attachment.name}</p>
+              {deletingId === attachment.id && (
+                <div>
+                  {" "}
+                  <Loader2 className="h-4 w-4 animate-spin" />
+                </div>
+              )}
+              {deletingId !== attachment.id && (
+                <button
+                  onClick={() => onDelete(attachment.id)}
+                  className="ml-auto hover:opacity-75 transition"
+                >
+                  <X className="h-4 w-4" />
+                </button>
+              )}
+            </div>
+          ))}
         </div>
       )}
       {isEditting && (
