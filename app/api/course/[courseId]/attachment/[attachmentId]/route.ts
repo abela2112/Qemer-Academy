c84df531d28ad3dd91785dce97315ef0d888354c
Abel Ayalew Abebe <103965027+abela2112@users.noreply.github.com>
2024-08-06 12:16:33 +0300

    update attachment routes

---
+++ app/api/course/[courseId]/attachment/[attachmentId]/route.ts
@@ -0,0 +1,29 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { courseId: string; attachmentId: string } }
+) {
+  try {
+    const { userId } = auth();
+    if (!userId) return new NextResponse("Unauthorized", { status: 401 });
+
+    const courseOwnerId = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId: userId,
+      },
+    });
+    if (!courseOwnerId)
+      return new NextResponse("Unauthorized", { status: 401 });
+    const attachment = await db.attachment.delete({
+      where: {
+        id: params.attachmentId,
+        courseId: params.courseId,
+      },
+    });
+    return NextResponse.json(attachment);
+  } catch (error) {}
+}
